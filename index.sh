452c7f5c05a2a7972524e626b655a17226c6ed14
afeiship <1290657123@qq.com>
2021-10-29 12:59:48 +0800

    feat: add gradle home

--- index.sh
+++ index.sh
@@ -2,7 +2,12 @@
 ## path:
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
+# java
 export JAVA_HOME=`/usr/libexec/java_home`;
 export PATH=${JAVA_HOME}/bin:$PATH;
+
+# gradle
+export GRADLE_HOME=$HOME/github/software/gradle/gradle-6.7.1
+export PATH=${GRADLE_HOME}/bin:$PATH;
 
 unset ROOT_PATH;
