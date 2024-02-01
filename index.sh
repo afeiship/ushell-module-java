3dedf890d78831676e0faca11e040b11b942baf7
afeiship <1290657123@qq.com>
2024-02-01 11:43:42 +0800

    feat: add java home

--- index.sh
+++ index.sh
@@ -3,5 +3,6 @@
 ROOT_PATH=$(dirname $BASH_SOURCE);
 
 source $ROOT_PATH/modules/01-jenv.sh;
+source $ROOT_PATH/modules/02-yw-java.sh;
 
 unset ROOT_PATH;
---
+++ modules/02-yw-java.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+# if ubuntu
+# $ update-alternatives --list java
+# /usr/lib/jvm/java-17-openjdk-amd64/bin/java
+
+if [ -f /etc/lsb-release ]; then
+  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+  export PATH=$PATH:$JAVA_HOME/bin
+fi
