35fab38ddd91350876af463cf5eb87bfac2c001e
afeiship <1290657123@qq.com>
2024-02-01 12:20:32 +0800

    Release 1.0.4

--- modules/02-yw-java.sh
+++ modules/02-yw-java.sh
@@ -5,6 +5,6 @@
 # /usr/lib/jvm/java-17-openjdk-amd64/bin/java
 
 if [ -f /etc/lsb-release ]; then
-  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
+  export JAVA_HOME=/usr/lib/jvm/java-17-openjdk-amd64
   export PATH=$PATH:$JAVA_HOME/bin
 fi
